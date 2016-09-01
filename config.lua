f8f040a6b737f3e3009b626cb66b1253cc8d4c9e
Jairo Arreola <eg6.2013@gmail.com>
2016-09-01 15:15:56 -0500

    first commit of the pokemon memory match game

---
+++ config.lua
@@ -0,0 +1,26 @@
+--
+-- For more information on config.lua see the Corona SDK Project Configuration Guide at:
+-- https://docs.coronalabs.com/guide/basics/configSettings
+--
+
+application =
+{
+	content =
+	{
+
+		graphicsCompatibility = 1;
+
+		width = 320,
+		height = 480, 
+		scale = "zoomStretch",
+		fps = 30,
+		
+		--[[
+		imageSuffix =
+		{
+			    ["@2x"] = 2,
+			    ["@4x"] = 4,
+		},
+		--]]
+	},
+}
