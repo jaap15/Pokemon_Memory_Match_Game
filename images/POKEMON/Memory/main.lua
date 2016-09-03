3700837b961ee3f10afd169ed8ffe065551a397d
Dburris13 <dab0021@uah.edu>
2016-09-03 13:26:24 -0500

    Add files via upload
    Version 1.1
    Added Menu, simple graphics, and restructured application with scenes
    Changed base resolution size to 1280x720, resized pokemon and match cards appropriately, added support for independent-resolutions
    Added an extra column to game scene, as well as two more pokemon to match
    
    To be done:
    Exit button from game scene doesn't work, update visuals, get application working on phone (missing files? build doesn't bring my background image with it for some reason), add animation for pokeball cards. show play time of application

---
+++ images/POKEMON/Memory/main.lua
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Memory Game
+local composer = require("composer")
+
+display.setStatusBar(display.HiddenStatusBar)
+
+composer.gotoScene("menu")
